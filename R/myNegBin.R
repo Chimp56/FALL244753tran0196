fb6e886fb24cd72648819fbe9ecbd044ca8c5e20
Vincent <47754918+Chimp56@users.noreply.github.com>
2024-09-23 13:06:52 -0500

    added lab5 function

---
+++ R/myNegBin.R
@@ -0,0 +1,14 @@
+#' @title Negative Binomial Distribution
+#'
+#' @param y number of failures
+#' @param r size
+#' @param p probability
+#'
+#' @return the probability of y failures before r successes
+#' @export
+#'
+#' @examples
+#' myNegBin(5,2,0.5)
+myNegBin=function(y,r,p){
+  choose(y-1,r-1)*p^r*(1-p)^(y-r)
+}
